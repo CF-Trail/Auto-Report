39496d33b3f8667b3e49ff7518162d3e4c3b747f
CF-Trail <106690765+CF-Trail@users.noreply.github.com>
2022-10-31 18:17:34 +0300

    minor update v2

--- main.lua
+++ main.lua
@@ -1,6 +1,49 @@
 repeat task.wait() until game:IsLoaded()
 
-words={hack={"Scamming","he's advertising hacks (viruses)"},cheat={"Scamming","he's advertising hacks (viruses)"},exploit={"Scamming","he's advertising hacks (viruses)"},script={"Scamming","he's advertising hacks (viruses)"},father={"Bullying","saying bad things about my parents"},orphan={"Bullying","saying bad things about my parents"},motherless={"Bullying","saying bad things about my parents"},parentless={"Bullying","saying bad things about my parents"},parents={"Bullying","saying bad things about my parents"},cancer={"Bullying","saying rude things to me, i can't take it anymore, i've had a really bad year"},kid={"Bullying","saying rude things to me, i can't take it anymore, i've had a really bad year"},bad={"Bullying","saying rude things to me, i can't take it anymore, i've had a really bad year"},['get a life']={"Bullying","saying rude things to me, i can't take it anymore, i've had a really bad year"},['no life']={"Bullying","saying rude things to me, i can't take it anymore, i've had a really bad year"},noob={"Bullying","saying rude things to me, i can't take it anymore, i've had a really bad year"},mods={"Bullying","saying rude things to me, i can't take it anymore, i've had a really bad year"},gay={"Bullying","saying bad things about other people, bullying them"},wizard={"Bullying","He said mean things to me"},gvae={"Bullying","saying bad things about other people, bullying them"},gae={"Bullying","saying bad things about other people, bullying them"},lgbt={"Bullying","saying bad things about other people, bullying them"},furr={"Bullying","saying bad things about other people, bullying them"},homo={"Bullying","saying bad things about other people, bullying them"},dumb={"Bullying","this is really mean! i've had a really bad year, my dog passed out and i just can't take it anymore!"},stupid={"Bullying","this is really mean! i've had a really bad year, my dog passed out and i just can't take it anymore!"},fat={"Bullying","this is really mean! i've had a really bad year, my dog passed out and i just can't take it anymore!"},ugly={"Bullying","this is really mean! i've had a really bad year, my dog passed out and i just can't take it anymore!"},kms={"Bullying","this is really mean! i've had a really bad year, my dog passed out and i just can't take it anymore!"},kvs={"Bullying","this is really mean! i've had a really bad year, my dog passed out and i just can't take it anymore!"},trash={"Bullying","this is really mean! i've had a really bad year, my dog passed out and i just can't take it anymore!"},suck={"Bullying","this is really mean! i've had a really bad year, my dog passed out and i just can't take it anymore!"},mad={"Bullying","this is really mean! i've had a really bad year, my dog passed out and i just can't take it anymore!"},bald={"Bullying","this is really mean! i've had a really bad year, my dog passed out and i just can't take it anymore!"},retard={"Bullying","this is really mean! i've had a really bad year, my dog passed out and i just can't take it anymore!"},cry={"Bullying","this is really mean! i've had a really bad year, my dog passed out and i just can't take it anymore!"},ratio={"Bullying","this is really mean! i've had a really bad year, my dog passed out and i just can't take it anymore!"},clown={"Bullying","this is really mean! i've had a really bad year, my dog passed out and i just can't take it anymore!"},['skill issue']={"Bullying","this is really mean! i've had a really bad year, my dog passed out and i just can't take it anymore!"},white={"Bullying","He's racist!"},black={"Bullying","He's racist!"},asia={"Bullying","He's racist!"},negr={"Bullying","He's racist!"},love={"Dating","he's dating in roblox"},['like you']={"Dating","he's dating in roblox"},russia={"Bullying","he talking about russia ukraine war"},ukraine={"Bullying","he talking about russia ukraine war"},youtube={"Offsite Links","he offsite links"},cringe={'Bullying','he said im cringe :('}}
+words = {
+    ['gay'] = 'Bullying',
+    ['lesbian'] = 'Bullying',
+    ['retard'] = 'Bullying',
+    ['noob'] = 'Bullying',
+    ['clown'] = 'Bullying',
+    ['get a life'] = 'Bullying',
+    ['getalife'] = 'Bullying',
+    ['no life'] = 'Bullying',
+    ['nolife'] = 'Bullying',
+    ['wizard'] = 'Bullying',
+    ['reports'] = 'Bullying',
+    ['father'] = 'Bullying',
+    ['mother'] = 'Bullying',
+    ['dumb'] = 'Bullying',
+    ['stupid'] = 'Bullying',
+    ['cringe'] = 'Bullying',
+    ['skill issue'] = 'Bullying',
+    ['parent'] = 'Bullying',
+    ['kid'] = 'Bullying',
+    ['child'] = 'Bullying',
+    ['trash'] = 'Bullying',
+    ['bozo'] = 'Bullying',
+    ['kys'] = 'Bullying',
+    ['die'] = 'Bullying',
+    ['killyou'] = 'Bullying',
+    ['loser'] = 'Bullying',
+    ['black'] = 'Bullying',
+    [' white '] = 'Bullying',
+    ['ez l'] = 'Bullying',
+    ['l ez'] = 'Bullying',
+    ['negro'] = 'Bullying',
+    ['nivver'] = 'Bullying',
+    ['negar'] = 'Bullying',
+    ['bad'] = 'Bullying',
+    ['worst'] = 'Bullying',
+    ['fat'] = 'Bullying',
+    ['hack'] = 'Scamming',
+    ['exploit'] = 'Scamming',
+    ['cheat'] = 'Scamming',
+    ['download'] = 'Offsite Links',
+    ['youtube'] = 'Offsite Links',
+    ['dizzy'] = 'Offsite Links',
+}
 
 if not game:GetService('ReplicatedStorage'):FindFirstChild('DefaultChatSystemChatEvents') or not game:GetService('ReplicatedStorage'):FindFirstChild('DefaultChatSystemChatEvents'):FindFirstChild('OnMessageDoneFiltering') then return end
 DCSCE = game:GetService('ReplicatedStorage'):FindFirstChild('DefaultChatSystemChatEvents')
@@ -42,8 +85,10 @@ end
 function handler(msg,speaker)
    for i,v in next, words do
       if string.match(string.lower(msg),i) then
-         players:ReportAbuse(players[speaker],v[1],v[2])
-         task.wait(0.2)
+        for i = 1,2 do
+         players:ReportAbuse(players[speaker],v,'He is breaking roblox TOS')
+         task.wait(0.5)
+        end
          if autoreportcfg.Webhook ~= nil and autoreportcfg.Webhook ~= '' and autoreportcfg.Webhook ~= ' ' then
          local data = 
          {
@@ -64,6 +109,16 @@ function handler(msg,speaker)
                          ["value"] = msg,
                          ["inline"] = true
                      },
+                     {
+                        ["name"] = "Offensive Part",
+                        ["value"] = i,
+                        ["inline"] = true
+                    },
+                    {
+                        ["name"] = "Reported For",
+                        ["value"] = v,
+                        ["inline"] = true
+                    },
                  },
                  ["footer"] = {
                      ["text"] = "\nIf you think this is a mistake: stfu"
